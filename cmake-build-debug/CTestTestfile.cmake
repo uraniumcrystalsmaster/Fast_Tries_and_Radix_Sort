fd60648614d7975210afc3c44cde2af020de7fe7
UraniumCM <uraniumcrystalsmaster@gmail.com>
2025-10-14 12:33:52 -0400

    Added Funnel Hash Map as a submodule dependency

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/urani/Documents/C++ Scripts UF/DSA/X-fast trie and radix sort project/fast-tries-and-radix-sort
+# Build directory: C:/Users/urani/Documents/C++ Scripts UF/DSA/X-fast trie and radix sort project/fast-tries-and-radix-sort/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/urani/Documents/C++ Scripts UF/DSA/X-fast trie and radix sort project/fast-tries-and-radix-sort/cmake-build-debug/Tests-b12d07c_include.cmake")
+subdirs("_deps/catch2-build")
---
+++ cmake-build-debug/Tests-b12d07c_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Users/urani/Documents/C++ Scripts UF/DSA/X-fast trie and radix sort project/fast-tries-and-radix-sort/cmake-build-debug/Tests-b12d07c_tests.cmake")
+  include("C:/Users/urani/Documents/C++ Scripts UF/DSA/X-fast trie and radix sort project/fast-tries-and-radix-sort/cmake-build-debug/Tests-b12d07c_tests.cmake")
+else()
+  add_test(Tests_NOT_BUILT-b12d07c Tests_NOT_BUILT-b12d07c)
+endif()
